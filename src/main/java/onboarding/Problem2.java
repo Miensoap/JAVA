5d665a370408527d68dc0a0079730019bd86daab
Miensoap <miensop51@gmail.com>
2023-08-07 22:02:45 +0900

    230807 2

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -1,8 +1,35 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
+        String answer="answer";
+         while (true) {
+            answer = decode(cryptogram);
+            if (answer.equals(cryptogram)){ // 전이랑 똑같으면 그만
+                break;
+            }
+            cryptogram = answer;
+        }
         return answer;
+    }
+
+    static String decode(String word){
+        StringBuilder sb= new StringBuilder();
+        sb.append(word);
+        int length=sb.length();
+        for (int i=0; i<length-1; i++ ){
+            if (sb.charAt(i)==sb.charAt(i+1)){
+                sb.deleteCharAt(i);
+                sb.deleteCharAt(i);
+//                length-=2;
+                return sb.toString();
+            }
+        }
+
+        return sb.toString();
     }
 }
