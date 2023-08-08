b8f9235245e90a66a23c8f9f552b207fa7818c60
Miensoap <miensop51@gmail.com>
2023-08-08 17:23:16 +0900

    230808 3

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -3,6 +3,15 @@ package onboarding;
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
+        for (int i=0; i<number+1; i++){
+            answer+=is_3(i);
+        }
         return answer;
+    }
+
+    static long is_3(int number){
+        String strnum = number+"";
+        long clap = strnum.chars().filter(c->c=='3' || c=='6' || c=='9').count();
+        return clap;
     }
 }
