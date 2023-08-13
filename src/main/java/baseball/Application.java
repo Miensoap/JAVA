e59e84d12e5ad80080c36aca90589cc28f899c32
Miensoap <miensop51@gmail.com>
2023-08-14 00:35:19 +0900

    0814

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+import java.util.stream.Stream;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameLauncher baseballgame = new GameLauncher();
+        baseballgame.run();
     }
 }
